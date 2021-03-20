b6d861e4a71b8be019becd0b5f906317ef175d27
Sai Pradeepti <saipradeepti14@gmail.com>
2021-03-20 17:36:37 +0530

    Adding app.js

--- 01 Form Validation/app.js
+++ 01 Form Validation/app.js
@@ -4,9 +4,9 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-const isValidEmail () => {
+const isValidEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test();
+    return re.test(String(email).toLowerCase());
 }
     
 
@@ -29,7 +29,16 @@ const showSuccess = (input) => {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     username.value === '' ? showError(username,'Enter username'):showSuccess(username);
-    email.value === '' ? showError(email,'Enter email'):showSuccess(email);
+    // if(email.value === '')
+    //     showError(email,'Enter email');
+    // else if(!isValidEmail(email.value)){
+    //     showError(email,'Enter valid email');
+    // }    
+    // else
+    //     showSuccess(email);
+    email.value === '' ? (showError(email,'Enter email')): (!isValidEmail(email.value) ? showError(email,'Enter valid email') : showSuccess(email))
+
     password.value === '' ? showError(password,'Enter password'):showSuccess(password);
+    
     password2.value === '' ? showError(password2,'Enter password again'):showSuccess(password2);
 })
