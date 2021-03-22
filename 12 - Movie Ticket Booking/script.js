d590252997927dbfdc301911ac82edd34f16fe3f
Sai Pradeepti <saipradeepti14@gmail.com>
2021-03-22 23:35:15 +0530

    Adding interactivity (app.js)

---
+++ 12 - Movie Ticket Booking/script.js
@@ -0,0 +1,33 @@
+const seatArrangement = document.querySelector('.seat__arrangement');
+const seatCount = document.querySelector('.seat__count');
+const totalCost = document.querySelector('.total__cost');
+const select = document.querySelector('select');
+let seatCost = document.querySelector('select').value;
+let count;
+
+// Updates Booking Details
+const updateBookingDetails = () => {
+    let seatsSelected = document.querySelectorAll('div.row .seat-selected');
+    count = seatsSelected.length;
+    seatCount.innerText = count;
+    totalCost.innerText = count * seatCost;
+}
+
+// Event Listner
+// Selects or unselects a seat in seat arrangement
+// Counts the seats selected
+seatArrangement.addEventListener('click', (e) => {
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('seat-taken') && !e.target.classList.contains('seat-infoStyle')){
+        e.target.classList.toggle('seat-selected');
+        updateBookingDetails();
+    }
+});
+
+// Sets the seat cost on change of movie select options
+select.addEventListener('change', () => {
+    seatCost = document.querySelector('select').value;
+    updateBookingDetails();
+});
+
+
+
