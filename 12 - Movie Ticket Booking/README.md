03f7416a490dd011fc96dd2da265bbfb6efa7d9e
Sai Pradeepti <saipradeepti14@gmail.com>
2021-03-23 17:36:46 +0530

    Adding Local Storage functionality

--- 12 - Movie Ticket Booking/README.md
+++ 12 - Movie Ticket Booking/README.md
@@ -1,10 +1,20 @@
 # Movie Ticket Booking
 
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Features](#features)
+
 ## Overview
 
 - Building Movie Ticket Booking using HTML, CSS and Vanilla JS
 
-### Desktop View
+### Screenshot
 
 ![](./img/Capture.PNG)
 
@@ -13,3 +23,12 @@
 - Live Site URL: [Click Here]()
 
 - [Design inspiration](https://dribbble.com/shots/4672745-Movie-Booking-App/attachments/4672745-Movie-Booking-App?mode=media)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
--- 12 - Movie Ticket Booking/script.js
+++ 12 - Movie Ticket Booking/script.js
@@ -2,8 +2,20 @@ const seatArrangement = document.querySelector('.seat__arrangement');
 const seatCount = document.querySelector('.seat__count');
 const totalCost = document.querySelector('.total__cost');
 const select = document.querySelector('select');
-let seatCost = select.value;
+const allRows = document.querySelectorAll('.row');
+let seatCost;
 let count;
+
+// adding IDs to rows and seats
+    allRows.forEach((row,index) => {
+        row.setAttribute('id',index);
+        const allSeatInRow = row.children;
+        Array.from(allSeatInRow).forEach((seat,index) => {
+            seat.setAttribute('id',index);
+        })
+    });
+
+
 
 // Updates Booking Details
 const updateBookingDetails = () => {
@@ -11,6 +23,48 @@ const updateBookingDetails = () => {
     count = seatsSelected.length;
     seatCount.innerText = count;
     totalCost.innerText = count * seatCost;
+}
+
+// Get seats selected
+const getSeatsSelected = () => {
+    let seatLocationList = [];
+    let seatsSelected =  document.querySelectorAll('div.row .seat-selected');
+    seatsSelected.forEach(seat => {
+        const seatNum = seat.getAttribute('id');
+        const rowNum = seat.parentElement.getAttribute('id');
+        seatLocationList.push([rowNum,seatNum])
+    })  
+    return seatLocationList;
+}
+
+// Updating Local Storage
+const updateLocalStorage = () => {
+
+    const bookingInfo = {
+        selectedIndex: select.options.selectedIndex,
+        seatsSelected : getSeatsSelected(),
+    }
+    
+    for(let property in bookingInfo){
+        localStorage.setItem(property,JSON.stringify(bookingInfo[property]))
+    }
+}
+
+// Load contents from local storage to UI
+const updateUI = () => {
+    select.options.selectedIndex = +(JSON.parse(localStorage.getItem('selectedIndex')));
+    const seatsSelectedAlready = JSON.parse(localStorage.getItem('seatsSelected'));
+    allRows.forEach((row,index) => {
+        seatsSelectedAlready.forEach(seatSelectedAlready => {
+            if(index === +seatSelectedAlready[0]){
+                const allSeatInRow = row.children;
+                Array.from(allSeatInRow).forEach((seat,i) => {
+                    if(i === +seatSelectedAlready[1])
+                        seat.classList.add('seat-selected');
+                })
+            }
+        })
+    });
 }
 
 // Event Listner
@@ -21,13 +75,22 @@ seatArrangement.addEventListener('click', (e) => {
         e.target.classList.toggle('seat-selected');
         updateBookingDetails();
     }
+    // Updating Local Storage
+    updateLocalStorage();
 });
 
 // Sets the seat cost on change of movie select options
 select.addEventListener('change', () => {
     seatCost = document.querySelector('select').value;
     updateBookingDetails();
+    updateLocalStorage();
 });
 
+// Load contents from the Local Storage
+document.addEventListener('DOMContentLoaded', () => {
+    updateUI();
+    seatCost = select.options[select.selectedIndex].value;
+    updateBookingDetails();
+});
 
 
